9917076ee53cc5cc6cf62bc001771a085d2056a1
Mikkko <dan_z@mail.ua>
2020-01-06 20:02:34 +0200

    Added controller, model and migration for TodoLists

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-01-03 00:25:45.
+    * Generated on 2020-01-06 18:01:03.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
