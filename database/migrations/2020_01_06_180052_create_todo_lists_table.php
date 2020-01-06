d328c75e462841ac82d39940b1582ca20f966a5a
Mikkko <dan_z@mail.ua>
2020-01-06 20:19:35 +0200

    Added relationships TodoLists->User in User and TodoList models and in migration

--- database/migrations/2020_01_06_180052_create_todo_lists_table.php
+++ database/migrations/2020_01_06_180052_create_todo_lists_table.php
@@ -1,29 +1,21 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateTodoListsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('todo_lists', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('todo_lists');
