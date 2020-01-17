deca25f3e976761306fc1a15a93a800444ede1e7
Mikkko <dan_z@mail.ua>
2020-01-17 16:00:54 +0200

    Realised REST architecture in TodoListSController

--- app/Http/Controllers/TodoListsController.php
+++ app/Http/Controllers/TodoListsController.php
@@ -1,10 +1,39 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\TodoList;
 use Illuminate\Http\Request;
+use Validator;
 
 class TodoListsController extends Controller
 {
-    //
+    public function index ()
+    {
+        $todolists = TodoList::getAllTodoListsForCurrentUser();
+        return view('todolist.index', compact('todolists'));
+    }
+
+    public function create ()
+    {
+        TodoList::NewTodoList()->save();
+        return redirect()->route('todolists.index')->with('success', 'New TodoList successfully created!');
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, ['title' => ['required', 'string', 'max:16']]);
+    }
+
+    public function edit(Request $request, $id)
+    {
+        $this->validator($request->all())->validate();
+        TodoList::editTodoList($id);
+        return redirect()->route('todolists.index')->with('success', 'TodoList title successfully edited!');
+    }
+
+    public function delete ($id)
+    {
+        TodoList::deleteTodoList($id);
+        return redirect()->route('todolists.index')->with('success', "TodoList $id successfully deleted!");
+    }
 }
