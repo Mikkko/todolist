5b6788cd18f11a5ffd30ef7d12352b0fd44ff495
Mikkko <dan_z@mail.ua>
2020-01-17 15:59:36 +0200

    Added REST methods to TodoList model

--- app/TodoList.php
+++ app/TodoList.php
@@ -6,7 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class TodoList extends Model
 {
-    public function user () {
+    protected $fillable = ['title', 'id', 'user_id'];
+
+    public function user ()
+    {
         return $this->belongsTo(User::class);
+    }
+
+    public static function getAllTodoListsForCurrentUser ()
+    {
+        return self::where('user_id', auth()->id())->get();
+    }
+
+    public static function NewTodoList () {
+        return self::create(['title' => 'New todolist', 'user_id' => auth()->id()]);
+    }
+
+    public static function deleteTodoList ($id)
+    {
+        return self::where('id', $id)->delete();
+    }
+
+    public static function editTodoList ($id)
+    {
+        return self::findOrFail($id)->update(['title'=>request('title')]);
     }
 }
