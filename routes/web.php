e74c563e500e282653514ce9f53f3b6753113153
Mikkko <dan_z@mail.ua>
2020-01-17 15:56:43 +0200

    Created todolist routing

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-Route::get('/', function () {
-    return view('todolist.index');
-})->middleware('auth');
+Route::get('/', 'TodoListsController@index')->name('todolists.index')->middleware('auth');
+Route::post('/todolist/create', 'TodoListsController@create')->name('todolist.create');
+Route::delete('/todolist/{id}/delete', 'TodoListsController@delete')->name('todolist.delete');
+Route::get('/todolist/{id}/edit', 'TodoListsController@edit')->name('todolist.edit');
 
 Auth::routes();
 
