3c3e5dd41291c5f5ead07d27c0f66dfa5dcb394a
Mikkko <dan_z@mail.ua>
2020-01-24 03:08:42 +0200

    Edit form hide/show

--- public/js/todoList.js
+++ public/js/todoList.js
@@ -1,6 +1,12 @@
 function showEditForm (id) {
     var todolist = ".todolist_" + id + " ";
     $(todolist + ".editTodoListForm").show();
-    $(todolist + ".todolist_title").hide()
+    $(todolist + ".todolist_title").hide();
     $(".todolist-menu").hide();
 }
+
+$(".fa-times").click(function () {
+    $(".editTodoListForm").hide();
+    $(".todolist_title").show();
+    $(".todolist-menu").show();
+});
