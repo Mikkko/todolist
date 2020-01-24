6cf92747f476fb63804676395dae9c0112be244c
Mikkko <dan_z@mail.ua>
2020-01-24 03:00:35 +0200

    Deleted active class from navbar

--- resources/views/templates/authenticationNavBar.blade.php
+++ resources/views/templates/authenticationNavBar.blade.php
@@ -1,9 +1,9 @@
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
+        <li class="nav-item">
             <a class="nav-link font-weight-bold" href="{{route('login')}}">Login</a>
         </li>
-        <li class="nav-item active">
+        <li class="nav-item">
             <a class="nav-link font-weight-bold" href="{{route('register')}}">Register</a>
         </li>
     </ul>
--- resources/views/templates/indexNavbar.blade.php
+++ resources/views/templates/indexNavbar.blade.php
@@ -1,6 +1,6 @@
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
+        <li class="nav-item">
             <a class="nav-link font-weight-bold" id="logoutLink">
                 Logout
             </a>
