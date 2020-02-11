59363f0c90fff1d4c19915d57bfc3399f43e0ffc
Mikkko <dan_z@mail.ua>
2020-02-11 18:15:43 +0200

    Added deadline validating, created deadline controller, renamed methods

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -4,24 +4,39 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Validator;
 use App\Task;
+use Carbon\Carbon;
 
 class TasksController extends Controller
 {
     public function create (Request $request, $id)
     {
-        $this->validator($request->all())->validate();
-        Task::NewTask($id)->save();
+        $this->createTaskValidator($request->all())->validate();
+        Task::newTask($id)->save();
         return redirect()->route('todolists.index')->with('success', 'New task successfully created!');
     }
 
-    protected function validator(array $data)
+    protected function createTaskValidator(array $data)
     {
         return Validator::make($data, ['title' => ['required', 'string', 'max:16']]);
+    }
+
+    protected function editDeadlineValidator(array $data)
+    {
+        $before = Carbon::now()->addMonth()->format('Y-m-d');
+        return Validator::make($data, ['deadline' => ['required', 'date', 'after:today', "before:$before"]]);
+    }
+
+    public function edit (Request $request, $id)
+    {
+        $deadline = $request['deadline'];
+        $this->editDeadlineValidator($request->all())->validate();
+        Task::editTask($id);
+        return redirect()->route('todolists.index')->with('success', "Deadline successfully set on $deadline!");
     }
 
     public function delete ($id)
     {
         Task::deleteTask($id);
-        return redirect()->route('todolists.index')->with('success', "Task $id successfully deleted!");
+        return redirect()->route('todolists.index')->with('success', "Task successfully deleted!");
     }
 }
