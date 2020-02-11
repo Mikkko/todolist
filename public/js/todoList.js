3c01c1187adcff9ff29b0e228cec39c488be7bbd
Mikkko <dan_z@mail.ua>
2020-02-11 18:09:52 +0200

    New functions for show and hide editing task form, function for completed tasks styles

--- public/js/todoList.js
+++ public/js/todoList.js
@@ -1,12 +1,40 @@
-function showEditForm (id) {
+function showEditTodoListForm (id) {
     var todolist = ".todolist_" + id + " ";
     $(todolist + ".editTodoListForm").show();
     $(todolist + ".todolist_title").hide();
     $(".todolist-menu").hide();
 }
 
-$(".fa-times").click(function () {
+function showEditTaskForm (id) {
+    var task = ".task_" + id + " ";
+    $(task + ".task_title").hide();
+    $(task + ".editTaskForm").show();
+    $(".task-menu").hide();
+}
+
+function completeTask (id)
+{
+    var task = ".task_" + id + " ";
+    var taskTitle = task + ".task_title";
+    var taskCheck = task + ".checkTask";
+
+    if($(taskCheck).is(":checked")) {
+        $(taskTitle).css('text-decoration', 'line-through');
+    }
+    else
+    {
+        $(taskTitle).css('text-decoration', 'none');
+    }
+}
+
+$(".cancel-editTodoList").click(function () {
     $(".editTodoListForm").hide();
     $(".todolist_title").show();
     $(".todolist-menu").show();
+});
+
+$(".cancel-editTask").click(function () {
+    $(".editTaskForm").hide();
+    $(".task-menu").show();
+    $(".task-checkbox").show();
 });
