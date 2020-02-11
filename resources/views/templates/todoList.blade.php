aa728d6c7ba80d2920839665c8d6180f8edce98b
Mikkko <dan_z@mail.ua>
2020-02-11 18:07:27 +0200

    Edited template

--- resources/views/templates/todoList.blade.php
+++ resources/views/templates/todoList.blade.php
@@ -1,45 +1,45 @@
 <div class="container-fluid mt-5">
     <div class="row justify-content-center">
-        <div class="col col-12 col-md-10 col-xl-4 todolist_{{$todolist->id}}">
+        <div class="col-12 col-md-10 col-xl-4 todolist_{{$todolist->id}} todolist">
 
             <div class="row thead-row">
-                <div class="col col-1">
+                <div class="col-1">
                     <i class="fas fa-clipboard-list fa-2x"></i>
                 </div>
                 <div class="col">
-                  <form class="editTodoListForm" action="todolist/{{$todolist->id}}/edit" class="col-12" method="get">
-                      <input name="title" type="text" class="col-6 col-sm-10" value="{{$todolist->title}}">
-                      <button type="submit" class="edit-todoListSubmit">
-                          <i class="fas fa-check fa-lg"></i>
-                      </button>
-                      <i class="fas fa-times fa-lg"></i>
-                  </form>
-                      <span class="todolist_title">{{$todolist->title}}</span>
+                    <form class="editTodoListForm" action="todolist/{{$todolist->id}}/edit" method="get">
+                        <input name="title" class="todoListTitleInput" type="text" value="{{$todolist->title}}">
+                        <button type="submit" class="edit-todoListSubmit">
+                            <i class="fas fa-check fa-lg"></i>
+                        </button>
+                        <i class="fas fa-times fa-lg cancel-editTodoList"></i>
+                    </form>
+                    <span class="todolist_title">
+                        {{$todolist->title}}
+                    </span>
                 </div>
-                <div class="col col-4 col-sm-2">
-                    <div class="todolist-menu">
-                        <i onclick="showEditForm({{$todolist->id}})" class="fas fa-pencil-alt col-5 edit-todoList"></i>
-                        <form action="todolist/{{$todolist->id}}/delete" method="post" class="delete-todoListForm">
-                            {{csrf_field()}}
-                            {{method_field('DELETE')}}
-                            <button type="submit" class="delete-todoListSubmit">
-                                <i class="fas fa-trash-alt col-5 delete-todoList"></i>
-                            </button>
-                        </form>
-                    </div>
+                <div class="col-4 col-sm-2 todolist-menu">
+                    <i onclick="showEditTodoListForm({{$todolist->id}})" class="fas fa-pencil-alt edit-todoList"></i>
+                    <form action="todolist/{{$todolist->id}}/delete" method="post" class="delete-todoListForm">
+                        {{csrf_field()}}
+                        {{method_field('DELETE')}}
+                        <button type="submit" class="delete-todoListSubmit">
+                            <i class="fas fa-trash-alt delete-todoList"></i>
+                        </button>
+                    </form>
                 </div>
             </div>
 
             <form action="/todolist/{{$todolist->id}}/task/create" method="post" class="create-taskForm">
                 {{csrf_field()}}
                 <div class="row tmiddle-row">
-                    <div class="col col-1">
+                    <div class="col-1">
                         <i class="fas fa-plus fa-2x"></i>
                     </div>
                     <div class="col">
-                        <input name="title" placeholder="Start typing here to create a task..." type="text" class="col" required>
+                        <input name="title" placeholder="Start typing here to create a task..." type="text" class="taskTitleInput" required>
                     </div>
-                    <div class="col col-4 col-sm-2 mr-1">
+                    <div class="col-4 col-sm-2 mr-1">
                         <button type="submit" class="createTaskSubmit">Add Task</button>
                     </div>
                 </div>
