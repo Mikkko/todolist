b83e4de6b16eeb8bd3ac177418c398efe5396fb3
Mikkko <dan_z@mail.ua>
2020-02-11 18:11:58 +0200

    Added form for editing task deadline

--- resources/views/templates/todoListTaskRow.blade.php
+++ resources/views/templates/todoListTaskRow.blade.php
@@ -1,15 +1,24 @@
-<div class="row task-row">
-    <div class="col col-1">
-        <input type="checkbox" class="checkTask">
+<div class="row task-row task_{{$task->id}}">
+    <div class="col-1 taskCheckBoxColumn">
+        <input type="checkbox" onclick="completeTask({{$task->id}})" class="checkTask">
     </div>
     <div class="col">
-        <span>
+        <span class="task_title text-justify-left">
             {{$task -> title}}
         </span>
+
+        <form class="editTaskForm" action="task/{{$task->id}}/edit" method="get">
+            <input type="date" name="deadline" value="{{$task->deadline}}">
+            <button type="submit" class="edit-taskSubmit">
+                <i class="fas fa-check fa-lg"></i>
+            </button>
+            <i class="fas fa-times fa-lg cancel-editTask"></i>
+        </form>
     </div>
-    <div class="col col-4 col-sm-2">
+    <div class="col-4 col-sm-2 task-menu">
+
         <i class="fas fa-sort fa-xs sort-task"></i>
-        <i class="fas fa-pencil-alt fa-xs edit-task"></i>
+        <i onclick="showEditTaskForm({{$task->id}})" class="fas fa-pencil-alt fa-xs edit-task"></i>
         <form action="/task/{{$task->id}}/delete" method="post" class="delete-taskForm">
           {{csrf_field()}}
           {{method_field('DELETE')}}
