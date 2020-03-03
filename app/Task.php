44b204a0280c72e94ec2a79e5ba99b0374fc1846
Mikkko <dan_z@mail.ua>
2020-03-03 14:18:22 +0200

    Added method for editing task status (complete or not)

--- app/Task.php
+++ app/Task.php
@@ -6,36 +6,46 @@ use Carbon\Carbon;
 
 class Task extends Model
 {
-  protected $fillable = ['title', 'todolist_id', 'deadline'];
+    protected $fillable = ['title', 'todolist_id', 'deadline', 'status'];
 
-  public function todolist ()
-  {
-      return $this->belongsTo(TodoList::class)->orWherenull('No tasks there...');
-  }
+    public function todolist ()
+    {
+        return $this->belongsTo(TodoList::class)->orWherenull('No tasks there...');
+    }
 
-  public static function newTask ($id)
-  {
-      $deadline = Carbon::now()->addDays(2)->format('Y-m-d');
-      return self::create(['title'=>request('title'), 'deadline'=>$deadline, 'todolist_id'=>$id]);
-  }
+    public static function newTask ($todolist_id)
+    {
+        $deadline = Carbon::now()->addDays(2)->format('Y-m-d'); 
+        return self::create(['title'=>request('title'), 'deadline'=>$deadline, 'todolist_id'=>$todolist_id]);;
+    }
 
-  public static function editTask ($id)
-  {
-      return self::findOrFail($id)->update(['deadline'=>request('deadline')]);
-  }
+    public static function editTaskDeadline ($id)
+    {
+        return self::findOrFail($id)->update(['deadline'=>request('deadline')]);
+    }
 
-  public static function deleteAllTasksFromTodoList ($id)
-  {
-      return self::where('todolist_id', $id)->delete();
-  }
+    public static function editTaskStatus ($id)
+    {
+        return self::findOrFail($id)->update(['status'=>request('status')]);
+    }
 
-  public static function deleteTaskWithDeadline () {
-      $date = Carbon::now()->format('Y-m-d');
-      return self::where('deadline', '<', $date)->delete();
-  }
+    public static function editTaskTitle ($id)
+    {
+        return self::findOrFail($id)->update(['title'=>request('title')]);
+    }
 
-  public static function deleteTask ($id)
-  {
-      return self::where('id', $id)->delete();
-  }
+    public static function deleteAllTasksFromTodoList ($id)
+    {
+        return self::where('todolist_id', $id)->delete();
+    }
+
+    public static function deleteTaskWithDeadline () {
+        $date = Carbon::now()->format('Y-m-d');
+        return self::where('deadline', '<', $date)->delete();
+    }
+
+    public static function deleteTask ($id)
+    {
+        return self::where('id', $id)->delete();
+    }
 }
